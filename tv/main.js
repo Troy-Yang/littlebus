af7eeed1119bb4deae76f783cf4684f16fc9e1f0
杨舟 <yangzhouemail@163.com>
2019-04-26 08:15:50 +0800

    using https

--- tv/main.js
+++ tv/main.js
@@ -6,7 +6,7 @@ var dp = new DPlayer({
     video: {
         // url: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4',
         // thumbnails: 'https://moeplayer.b0.upaiyun.com/dplayer/hikarunara_thumbnails.jpg',
-        url: 'http://livepull.troyyangdev.cn/live/myenglishtutor.m3u8',
+        url: 'https://livepull.troyyangdev.cn/live/myenglishtutor.m3u8',
         type: 'hls'
     }
 });
