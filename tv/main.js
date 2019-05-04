d145e094ed99d916975b339c01d71f3823a783dc
yang zhou <yangzhouemail@163.com>
2019-05-04 17:22:18 +0800

    add danmaku and more qulity

--- tv/main.js
+++ tv/main.js
@@ -1,21 +1,35 @@
 var dp = new DPlayer({
     container: document.getElementById('dplayer1'),
     preload: 'none',
-    // screenshot: true,
+    lang: 'zh-cn',
+    screenshot: true,
     live: true,
     video: {
         // url: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4',
-        // thumbnails: 'https://moeplayer.b0.upaiyun.com/dplayer/hikarunara_thumbnails.jpg',
-        url: 'https://livepull.troyyangdev.cn/live/myenglishtutor.m3u8',
-        type: 'hls'
+        quality: [
+            // {
+            //     name: '原始',
+            //     url: 'https://livepull.troyyangdev.cn/live/myenglishtutor.m3u8',
+            //     type: 'hls'
+            // },
+            {
+                name: '标清',
+                url: 'https://livepull.troyyangdev.cn/live/myenglishtutor_900.m3u8',
+                type: 'hls'
+            }, 
+            {
+                name: '流畅',
+                url: 'https://livepull.troyyangdev.cn/live/myenglishtutor_550.m3u8',
+                type: 'hls'
+            }],
+        defaultQuality: 0,
+    },
+    danmaku: {
+        id: 'demo',
+        api: 'http://littlebus.troyyangdev.cn:1207/'
     }
 });
 
 dp.on('error', function (e) {
     console.log('player ended' + e);
 });
-
-
-//rtmp://livepull.troyyangdev.cn/live/myenglishtutor 
-//http://livepull.troyyangdev.cn/live/myenglishtutor.m3u8
-//http://livepull.troyyangdev.cn/live/myenglishtutor.flv
