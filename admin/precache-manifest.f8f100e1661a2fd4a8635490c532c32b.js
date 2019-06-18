5ab3c3d80d62eb134b8928829983022ada6042c9
yang zhou <yangzhouemail@163.com>
2019-06-18 23:21:34 +0800

    start to use react admin

---
+++ admin/precache-manifest.f8f100e1661a2fd4a8635490c532c32b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe791985d04436628e4aff12bed59da",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "ba410258c429b7051cf2",
+    "url": "/admin/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d4b08a0c0b55e09029a1",
+    "url": "/admin/static/js/2.25cb88af.chunk.js"
+  },
+  {
+    "revision": "ba410258c429b7051cf2",
+    "url": "/admin/static/js/main.7eb18978.chunk.js"
+  },
+  {
+    "revision": "7c4f45329e47798217a3",
+    "url": "/admin/static/js/runtime~main.b3695f6e.js"
+  }
+]);
